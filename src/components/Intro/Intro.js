b8b25b9371d54fee3cc1d83b69dbe9e19a09db12
Unlovingmotherboard <jmhern25@yahoo.com>
2020-02-03 21:56:14 -0600

    Started working on a super simple layout with animejs. Will continue to expand on this idea.

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,22 +1,62 @@
 import React from "react"
+import RAnime from "react-anime"
+import anime from "animejs"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
 import "./IntroStyle.css"
 
-import headShot from "../../images/P1200423.jpg"
 
 const Intro = () => {
+    const me1 = () => {
+        anime({
+            targets: '#introDiv',
+            duration: 2000,
+            backgroundColor: '#FFF',
+            easing: 'easeInOutQuad'
+        });
 
-  return (
-    <>
-      <div id="IntroDIV" alt="myheadShot">
-        <div id="headShotDiv">
-            <img id="headShot" src={headShot}></img>
-        </div> 
-      </div>
-    </>
-  )
+        anime({
+            targets: '.controlBoxText',
+            duration: 2000,
+            color: '#FFF',
+            easing: 'easeInOutQuad'
+        });
+    }
+
+    const me2 = () => {
+        anime({
+            targets: '#introDiv',
+            duration: 2000,
+            backgroundColor: '#b4eb34',
+            easing: 'easeInOutQuad'
+
+        });
+        anime({
+            targets: '.controlBoxText',
+            duration: 2000,
+            color: '#000000',
+            easing: 'easeInOutQuad'
+        });
+    }
+    return (
+           <div id="introDiv">
+               <div id="controlBox">
+                   <div>
+                   <h1 onClick={me1} className="controlBoxText">Me1</h1>
+                   </div>
+                   <div>
+                   <h1 onClick={me2} className="controlBoxText">Me2</h1>
+                   </div>
+                   <div>
+                   <h1 className="controlBoxText">Me</h1>
+                   </div>
+                   <div>
+                   <h1 className="controlBoxText">Me</h1>
+                   </div>
+               
+               </div>
+           </div>
+    )
 }
 
 
