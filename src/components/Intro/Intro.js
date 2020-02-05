d736be0ee198c879f40daf83fc3565fa516be766
Unlovingmotherboard <jmhern25@yahoo.com>
2020-02-04 19:18:29 -0600

    started looking into how z-index works. I have started working on how the color transitions will work

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -11,7 +11,7 @@ const Intro = () => {
         anime({
             targets: '#introDiv',
             duration: 2000,
-            backgroundColor: '#FFF',
+            backgroundColor: '#000000',
             easing: 'easeInOutQuad'
         });
 
@@ -21,41 +21,45 @@ const Intro = () => {
             color: '#FFF',
             easing: 'easeInOutQuad'
         });
-    }
 
-    const me2 = () => {
         anime({
-            targets: '#introDiv',
+            targets: '.circle1',
             duration: 2000,
-            backgroundColor: '#b4eb34',
-            easing: 'easeInOutQuad'
-
-        });
-        anime({
-            targets: '.controlBoxText',
-            duration: 2000,
-            color: '#000000',
+            scale: 20,
+            fill: '#FFF',
             easing: 'easeInOutQuad'
         });
+
+        anime({
+            targets: '.circleSVG',
+            duration: 2000,
+            fill: '#b4eb34',
+            easing: 'easeInOutQuad'
+        });
     }
+
     return (
-           <div id="introDiv">
-               <div id="controlBox">
-                   <div>
-                   <h1 onClick={me1} className="controlBoxText">Me1</h1>
-                   </div>
-                   <div>
-                   <h1 onClick={me2} className="controlBoxText">Me2</h1>
-                   </div>
-                   <div>
-                   <h1 className="controlBoxText">Me</h1>
-                   </div>
-                   <div>
-                   <h1 className="controlBoxText">Me</h1>
-                   </div>
-               
-               </div>
-           </div>
+        <div id="introDiv">
+             <div className="scene">
+                <div id="circle">
+                    <svg className="circle1" width="200" height="200">
+                        <circle className="circleSVG" cx="100" cy="100" r="90" />
+                    </svg>
+                </div>
+            </div>
+
+            <div id="controlBox">
+                <div>
+                    <h1 onClick={me1} className="controlBoxText">About Me</h1>
+                </div>
+                <div>
+                    <h1 className="controlBoxText">Projects</h1>
+                </div>
+                <div>
+                    <h1 className="controlBoxText">Contact</h1>
+                </div>
+            </div>
+        </div>
     )
 }
 
